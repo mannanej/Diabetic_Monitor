e05ba9ce22e98d84d0e300417860cf4d0279e157
Eddie Mannan <eddiemannan@gmail.com>
2025-11-07 15:10:49 -0500

    MAde changes to use an APi instead

--- main_v2.py
+++ main_v2.py
@@ -3,6 +3,7 @@ import os
 import csv
 import time
 import shlex
+import pickle
 import subprocess
 from pydexcom import Dexcom
 from subprocess import call
@@ -10,20 +11,16 @@ from collections import deque
 from datetime import datetime
 # import Adafruit_BBIO.GPIO as GPIO
 ####################################################################################################################################
-MAX_CAPACITY = 25920
-####################################################################################################################################
 # File Name: main.py
 # Author: Eddie Mannan
 # Date Created: 06NOV2025
-# Date last edited: 06NOV2025
+# Date last edited: 07NOV2025
 # Topic: ECE434 Embedded Linux, Dr. Yoder
 # Project: Diabetic Monitor
 # Description: This program will look at an Excel sheet of blood sugar values, crunch some numbers, then display it in multiple ways
 # To see the full project, visit my Hackster.io page: https://www.hackster.io/eddiemannan/dexcom-g6-diabetic-monitor-3f7af7
 # Info on Dexcom API: https://gagebenne.github.io/pydexcom/pydexcom.html#Dexcom
 # 129,600 minutes in 90 days | sugar reading every 5 minutes | 25,920 reading over a A1C period (90 days)
-
-# Maybe try and get a sugar value, add it to a deque, pickle and unpickle it to use the values for an A1C
 ####################################################################################################################################
 # For the LEDs with 220 Ohm resistors:
 #     Red is P9_11
@@ -41,160 +38,96 @@ MAX_CAPACITY = 25920
 #     LED is P9_16
 #     MISO is P9_29
 ####################################################################################################################################
+MAX_CAPACITY = 25920
+SUGAR_Q = deque(maxlen = MAX_CAPACITY)
+DEXCOM_USERNAME = ""
+DEXCOM_PASSWORD = ""
+####################################################################################################################################
+class DexcomInfo:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        return
+####################################################################################################################################
+class A1CInfo:
+    def __init__(self, sugars):
+        self.sugars = sugars
+        return
+####################################################################################################################################
 def main():
     # This section will get the file we need with its path and open it with a csv reader
     systemRunningLED(True)
     startTime = time.perf_counter()
     
-    
-    # dexcom = Dexcom(username="username", password="password") # `region="ous"` if outside of US, `region="jp"` if Japan
-    # dexcom = Dexcom(username="+11234567890", password="password") # phone number
-    dexcom = Dexcom(username="eddiemannan@gmail.com", password="P!glet6386") # email address
-    # dexcom = Dexcom(account_id="12345678-90ab-cdef-1234-567890abcdef", password="password") # account ID (advanced)
+    dexcom = Dexcom(username = DEXCOM_USERNAME, password = DEXCOM_PASSWORD) # email address
     glucose_reading = dexcom.get_current_glucose_reading()
-    print(glucose_reading)
     
-    
-    # pathName = os.getcwd() + "/ExcelFiles/"
-    # newestFile = getNewestFile(pathName)
-    # pathName += newestFile
-    # filename = open(pathName, "r")
-    # file = csv.reader(filename)
-    # This section will get the column of sugar values
-    # sugars = getSugarsFromFile(file)
-    # This section will get the date
-    # currentDate = getDate(newestFile)
-    currentDate = glucose_reading.datetime
-    # This section will get the time
-    # currentTime = getTimeEastern(newestFile)
-    currentTime = "Get Time From The currentDate. It's datetime"
-    # This section will get the A1C value from these sugars
-    # a1c = getA1C(sugars)
-    a1c = glucose_reading.mg_dl
     # This section will get the current sugar
-    # currentSugar = sugars[-1]
     currentSugar = glucose_reading.value
-    # This section will sort the sugars from lowest to highest
-    # sugars.sort()
+    SUGAR_Q.append(currentSugar)
+    # This section will get the date
+    currentDate = glucose_reading.datetime
+    # This section will get the A1C value from these sugars
+    a1c = getA1C(SUGAR_Q)
     # This section will get the Highest sugar
-    # highest = sugars[-1]
-    highest = 400
+    highest = max(SUGAR_Q)
     # This section will get the lowest sugar
-    # lowest = sugars[0]
-    lowest = 50
+    lowest = min(SUGAR_Q)
+    # This section will save the current Sugar array
+    saveA1CInfo(SUGAR_Q)
 
     endTime = time.perf_counter()
     runTime = endTime - startTime
     # This section will display the info to the LCD screen
-    subprocess.call(shlex.split(f"./text.sh {currentDate.strftime('%m-%d-%Y')} {currentTime} {round(a1c, 1)} {currentSugar} {highest} {lowest} {round(runTime, 2)}"))
+    # subprocess.call(shlex.split(f"./text.sh {currentDate.strftime('%m-%d-%Y')} {currentDate.strftime('%I:%M:%S %p')} {round(a1c, 1)} {currentSugar} {highest} {lowest} {round(runTime, 2)}"))
     # This section will handle the LEDs
     sugarLevelLED(currentSugar)
     systemRunningLED(False)
     ########## General print statements for debugging ##########
-    # print(sugars)
-    # print(len(sugars))
-    # print("Date: ", currentDate.strftime('%m-%d-%Y'))
-    # print("Time: ", currentTime)
-    # print("A1C: ", round(a1c, 1))
-    # print("Current: ", currentSugar)
-    # print("Highest: ", highest)
-    # print("Lowest: ", lowest)
-    # print("Runtime: ", round(runTime, 2), "seconds")
+    print("Date: ", currentDate.strftime('%m-%d-%Y'))
+    print("Time: ", currentDate.strftime('%I:%M:%S %p'))
+    print("A1C: ", round(a1c, 1))
+    print("Current: ", currentSugar)
+    print("Highest: ", highest)
+    print("Lowest: ", lowest)
+    print("Runtime: ", round(runTime, 2), "seconds")
+    return
 ####################################################################################################################################
-def getNewestFile(pathName):
-    # Get an array of file names
-    fileNames = os.listdir(pathName)
-    # Set the "oldest day" as 01/01/2000
-    newestDate = datetime(2000, 1, 1, 1, 00, 00)
-    newestFile = ""
-    # Iterate through the list and save the most recent file
-    for i in range(0, len(fileNames)):
-        if (fileNames[i].endswith(".csv")):
-            currentDate = getDate(fileNames[i])
-            if ((currentDate - newestDate).total_seconds() > 0):
-                newestDate = currentDate
-                newestFile = fileNames[i]
-    return newestFile
+def saveDexcomInfo(username, password):
+    state = DexcomInfo(username, password)
+    with open("bins/DexcomInfo.bin", "wb") as file:
+        pickle.dump(state, file)
+    return
 ####################################################################################################################################
-def getSugarsFromFile(file):
-    # Create a couple starter variables
-    tempArray = []
-    skip = 0
-    index = 0
-    # Skip the first 10 rows, they dont have any useful data
-    for row in file:
-        if (skip <= 10):
-            skip += 1
-        else:
-            # Grabs every 7 cells, but since the multiples of 7 switch between even and odd, we only need every other 7th
-            for col in row:
-                if (index % 7 == 0 and index % 2 != 0):
-                    # If sugar is above 400, its stored as "High", replace it with a real number
-                    if (col == "High"):
-                        tempArray.append(450)
-                    else:
-                        tempArray.append(int(col))
-                index += 1
-    return tempArray
+def loadDexcomInfo():
+    global DEXCOM_USERNAME
+    global DEXCOM_PASSWORD
+    
+    with open("bins/DexcomInfo.bin", "rb") as file:
+        state = pickle.load(file)
+    DEXCOM_USERNAME = state.username
+    DEXCOM_PASSWORD = state.password
+    return
+####################################################################################################################################
+def saveA1CInfo(sugars):
+    state = A1CInfo(sugars)
+    with open("bins/A1CInfo.bin", "wb") as file:
+        pickle.dump(state, file)
+    return
+####################################################################################################################################
+def loadA1CInfo():
+    global SUGAR_Q
+    
+    with open("bins/A1CInfo.bin", "rb") as file:
+        state = pickle.load(file)
+    SUGAR_Q = state.sugars
+    return
 ####################################################################################################################################
 def getA1C(sugars):
     averageSugar = sum(sugars) / len(sugars)
     # Plug in our average sugar value to calulate an A1C
     a1c = (41.6 + averageSugar) / 28.7
     return a1c
-####################################################################################################################################
-def getDate(fileName):
-    # Make sure the file is a .csv, then return the date that is saved in the file name
-    if fileName.endswith(".csv"):
-        currentDate = datetime(year = int(fileName[29:33]), month = int(fileName[34:36]), day = int(fileName[37:39]), hour = int(fileName[40:42]), minute = int(fileName[42:44]), second = int(fileName[44:46]))
-    return currentDate
-####################################################################################################################################
-def getTimeEastern(fileName):
-    # Turns the time into Eastern time
-    if fileName.endswith(".csv"):
-        hour = fileName[40:42]
-        minute = fileName[42:44]
-        second = fileName[44:46]
-        if (hour == "04"):
-            hour = "23"
-        elif (hour == "03"):
-            hour = "22"
-        elif (hour == "02"):
-            hour = "21"
-        elif (hour == "01"):
-            hour = "20"
-        elif (hour == "00"):
-            hour = "19"
-        else:
-            hour = str(int(hour) - 5)
-        # easternTimeMilitary = hour + "::" + minute + "::" + second
-        if (hour == "13"):
-            hour = "1"
-        elif (hour == "14"):
-            hour = "2"
-        elif (hour == "15"):
-            hour = "3"
-        elif (hour == "16"):
-            hour = "4"
-        elif (hour == "17"):
-            hour = "5"
-        elif (hour == "18"):
-            hour = "6"
-        elif (hour == "19"):
-            hour = "7"
-        elif (hour == "20"):
-            hour = "8"
-        elif (hour == "21"):
-            hour = "9"
-        elif (hour == "22"):
-            hour = "10"
-        elif (hour == "23"):
-            hour = "11"
-        elif (hour == "00"):
-            hour = "12"
-        easternTimeStandard = hour + "::" + minute + "::" + second
-        currentTime = datetime.strptime(easternTimeStandard, '%H::%M::%S').time()
-    return currentTime
 ####################################################################################################################################
 def sugarLevelLED(currentSugar):
 #     # Control the different LEDs based on current sugar level
@@ -205,17 +138,17 @@ def sugarLevelLED(currentSugar):
 #     GPIO.setup(GREEN, GPIO.OUT)
 #     GPIO.setup(BLUE, GPIO.OUT)
 #     # Sugar is HIGH
-#     if (currentSugar >= 170):
+#     if (currentSugar >= 180):
 #         GPIO.output(RED, GPIO.HIGH)
 #         GPIO.output(GREEN, GPIO.LOW)
 #         GPIO.output(BLUE, GPIO.LOW)
 #     # Sugar is NORMAL
-#     elif (currentSugar < 170 and currentSugar > 70):
+#     elif (currentSugar < 180 and currentSugar > 80):
 #         GPIO.output(RED, GPIO.LOW)
 #         GPIO.output(GREEN, GPIO.HIGH)
 #         GPIO.output(BLUE, GPIO.LOW)
 #     # Sugar is LOW
-#     elif (currentSugar <= 70):
+#     elif (currentSugar <= 80):
 #         GPIO.output(RED, GPIO.LOW)
 #         GPIO.output(GREEN, GPIO.LOW)
 #         GPIO.output(BLUE, GPIO.HIGH)
@@ -239,6 +172,8 @@ def systemRunningLED(running):
 # This is a call to main to get the ball rolling
 if __name__ == '__main__':
     # newExcelPull = call("./getNewExcel.sh", shell=True)
+    loadDexcomInfo()
+    loadA1CInfo()
     main()
-# END FILE
 ####################################################################################################################################
+# END FILE
